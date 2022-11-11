abadec6cd5745654e7aa28c91a3c749e517237ad
caioalbert <caioalberto2104@gmail.com>
2022-11-11 00:45:42 -0300

    inplementação do modal puro

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,6 +102,44 @@
       });
     }
 
+    // obtendo o modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("suite1");
+var btn2 = document.getElementById("suite2");
+var btn3 = document.getElementById("suite3");
+var btn4 = document.getElementById("suite4");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+btn3.onclick = function() {
+  modal.style.display = "block";
+}
+btn4.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
     // Main.
     $t._update = function () {
       pos = 0;
