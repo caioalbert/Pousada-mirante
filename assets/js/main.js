58f66f8dc9a957cf3e9d70ca4ca9dbc3c0404fce
Bren-Dev <brenda_nogueira_pe@hotmail.com>
2022-11-12 13:06:11 -0300

    removendo itens desnecessários e ajustando manipulação da DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,42 +103,32 @@
     }
 
     // obtendo o modal
-var modal = document.getElementById("myModal");
+    var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn1 = document.getElementById("suite1");
-var btn2 = document.getElementById("suite2");
-var btn3 = document.getElementById("suite3");
-var btn4 = document.getElementById("suite4");
+    // Get the button that opens the modal
+    var btn = document.getElementsByClassName("suite");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    console.log(btn);
+    // When the user clicks the button, open the modal
+    for (let i = 0; i < btn.length; i++) {
+      btn[i].onclick = function () {
+        modal.style.display = "block";
+      };
+    }
 
-// When the user clicks the button, open the modal 
-btn1.onclick = function() {
-  modal.style.display = "block";
-}
-btn2.onclick = function() {
-  modal.style.display = "block";
-}
-btn3.onclick = function() {
-  modal.style.display = "block";
-}
-btn4.onclick = function() {
-  modal.style.display = "block";
-}
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
 
     // Main.
     $t._update = function () {
